268f4bc0074c2f51798064af5f7ea97ca266c7bc
taeubelnina <31923929+taeubelnina@users.noreply.github.com>
2018-11-12 19:23:50 +0100

    20181023 - SemaphoreDemo

---
+++ 20181023 - ThreadSemaphore/src/SemaphoreDemo.java
@@ -0,0 +1,38 @@
+import java.sql.Time;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class SemaphoreDemo {
+
+    static Semaphore semaphore = new Semaphore(2, true);
+
+    static  Runnable r = new Runnable() {
+
+        @Override
+        public void run() {
+            while(true){
+                try{
+                    semaphore.acquire();
+                    try{
+                        System.out.println("Thread=" + Thread.currentThread().getName() + ", Available Permits=" + semaphore.availablePermits());
+                        TimeUnit.SECONDS.sleep(2);
+                    }finally{
+                        semaphore.release();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }
+    };
+
+    public static void main(String[] args) {
+
+        new Thread(r).start();
+        new Thread(r).start();
+        new Thread(r).start();
+
+    }
+}
